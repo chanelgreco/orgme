1b52614583dd3f363970ecbc867d819ba4597142
Chanel Greco <chanel.d.greco@gmail.com>
2018-09-21 17:42:34 +0200

    First refactoring, extracting seperate components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ActivityDashboard from './Activity/ActivityDashboard';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<ActivityDashboard />, document.getElementById('root'));
 registerServiceWorker();
