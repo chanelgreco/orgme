4a1f40de0475b8a5052af937145676aad07e1825
Chanel Greco <chanel.d.greco@gmail.com>
2018-09-23 14:03:19 +0200

    Customize color pallette.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
 import ActivityDashboard from './Activity/ActivityDashboard';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#5e35b1'
+    },
+    secondary: {
+      main: '#ffff00'
+    }
+  }
+});
+
+const App = props => (
+  <MuiThemeProvider theme={theme}>
+    <ActivityDashboard />
+  </MuiThemeProvider>
+);
 
 ReactDOM.render(<ActivityDashboard />, document.getElementById('root'));
 registerServiceWorker();
